95dc2f0d208307bf8efdc8063c6a83b6e1ada797
Muhammad Ali <nu.muhammad.ali@gmail.com>
2014-06-26 19:11:07 +0500

    initial commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140625114638) do
+ActiveRecord::Schema.define(version: 20140626120040) do
 
   create_table "microposts", force: true do |t|
     t.string   "content"
@@ -21,6 +21,24 @@ ActiveRecord::Schema.define(version: 20140625114638) do
   end
 
   add_index "microposts", ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
+
+  create_table "models", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "models", ["email"], name: "index_models_on_email", unique: true
+  add_index "models", ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
 
   create_table "relationships", force: true do |t|
     t.integer  "follower_id"
