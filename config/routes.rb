7c7932ef973ff627f341c8e197e6d508c076331f
ali <muhammad.ali@tkxel.com>
2014-06-26 15:59:02 +0500

    Initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,21 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/custom"
-  get "static_pages/about_us"
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :users
+  resources :sessions,      only: [:new, :create, :destroy]
+  resources :microposts,    only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
+  root  'static_pages#home'
+  match '/signup',  to: 'users#new',            via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about_us',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+  match '/users' , to: 'users#index' , via: 'get'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
